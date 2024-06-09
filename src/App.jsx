cbb220be7221414f156a3249d9f8a1632143e7f2
sonhd2k <sonhd2k@gmail.com>
2024-06-10 00:46:33 +0700

    add_import_eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import Button from "@mui/material/Button";
-import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
-import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
-import HomeIcon from "@mui/icons-material/Home";
-import { pink } from "@mui/material/colors";
+import Button from '@mui/material/Button'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import HomeIcon from '@mui/icons-material/Home'
+import { pink } from '@mui/material/colors'
+
 
 function App() {
   return (
@@ -23,7 +24,7 @@ function App() {
       <HomeIcon color="disabled" />
       <HomeIcon sx={{ color: pink[900] }} />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
