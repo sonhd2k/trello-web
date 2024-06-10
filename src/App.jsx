f0534106373557403ed930e9793f13b64c61e564
sonhd2k <sonhd2k@gmail.com>
2024-06-10 22:18:45 +0700

    config_mui_theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
 
 
 function App() {
   return (
     <>
       <div>Thanh Sơn</div>
+      <Typography variant='h2' color='text.secondary'>Phương Ly</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
