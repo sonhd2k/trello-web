7bf707d55c2d30b01d62ed86ef4e83f817fd2c56
sonhd2k <sonhd2k@gmail.com>
2024-06-27 20:56:25 +0700

    refactor_project_structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,10 @@
-import {
-  useColorScheme
-} from '@mui/material/styles'
-// import useMediaQuery from '@mui/material/useMediaQuery'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import Box from '@mui/material/Box'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-import Container from '@mui/material/Container'
-
-
-function ModeSelect() {
-  const { mode, setMode } = useColorScheme()
-
-  const handleChange = (event) => {
-    const SelectMode=event.target.value
-    // console.log(SelectMode);
-    setMode(SelectMode)
-
-  }
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="lable-selcet-dark-light-mode">Age</InputLabel>
-      <Select
-        labelId="lable-selcet-dark-light-mode"
-        id="demo-select-small"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-
-        <MenuItem value='light'>
-          <div style={{ display:'flex', alignItems:'center', gap:'8px' }}>
-            <LightModeIcon fontSize='small'/>
-          Light
-          </div>
-
-        </MenuItem>
-        <MenuItem value='dark' >
-          <Box sx={{ display:'flex', alignItems:'center', gap:1 }}>
-            <DarkModeOutlinedIcon fontSize='small'/>
-          Dark
-          </Box>
-
-        </MenuItem>
-        <MenuItem value='system'>
-          <Box sx={{ display:'flex', alignItems:'center', gap:2 }}>
-            <SettingsBrightnessIcon fontSize='small'/>
-          System
-          </Box>
-
-        </MenuItem>
-      </Select>
-    </FormControl>
-  )
-}
-
+import Board from './pages/Users/_id'
 
 function App() {
   return (
-    <Container disableGutters maxWidth={false} sx={{ height:'100vh' }}>
-      <Box sx={{
-        backgroundColor :'primary.light',
-        width:'100%',
-        height:(theme) => theme.trello.appBarHeight,
-        display:'flex',
-        alignItems:'center'
-      }}>
-        <ModeSelect/>
-      </Box >
-      <Box sx={{
-        backgroundColor :'primary.dark',
-        width:'100%',
-        height:(theme) => theme.trello.boardBarHeight,
-        display:'flex',
-        alignItems:'center'
-      }}>
-        Board Bar
-      </Box>
-      <Box sx={{
-        backgroundColor:'primary.main',
-        width:'100%',
-        height:(theme)=> `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
-        display:'flex',
-        alignItems:'center'
-      }}>
-        Board Content
-      </Box>
-    </Container>
+    <>
+      <Board/>
+    </>
   )
 }
 
